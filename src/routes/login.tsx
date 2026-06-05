d7894f463935a6e9c83430781fb11c63ee673b8f
espacomulti93-cpu <288747587+espacomulti93-cpu@users.noreply.github.com>
2026-06-05 16:38:02 -0300

    chore: delete login route and update routeTree

--- src/routes/login.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Navigate } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/login")({
-  component: LoginPageRedirect,
-});
-
-function LoginPageRedirect() {
-  return <Navigate to="/dashboard" replace />;
-}
